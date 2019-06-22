f96d6f6f866397f3e5b6f75e8f89b42e2d02b8f0
eddominguez <51214743+eddominguez@users.noreply.github.com>
2019-06-21 23:29:04 -0500

    Add files via upload

---
+++ SearchFunction.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class SearchFunction {
+	
+	public static void main(String[] args) {
+		Scanner userIn = new Scanner(System.in);//Scanner is created
+		
+		System.out.println("What question are you searching for?");
+		
+		String readInput = userIn.nextLine();
+		
+		
+		for (int i = 0; i <l ength; i++) {
+			question[i] = String.valueOf(userIn.nextInt());
+			
+		}
+		return question;
+		
+		//if question matches, return the question
+		boolean searchFor(String[] question searchQuestion) {
+			return Arrays.asList(searchQuestion.question)
+		}
+		
+		
+	}
+
+}
